46e7a4c458af3d7caa18ff398c594c3c82d4624f
Victor Hidrogo <vhidrogo@uci.edu>
2025-11-04 11:11:44 -0800

    feat: add model to track LLM API requests

--- tracker/models/__init__.py
+++ tracker/models/__init__.py
@@ -1,3 +1,4 @@
 from .job import Job
+from .llmrequestlog import LlmRequestLog
 from .choices import JobLevel, JobRole, WorkSetting
 from .requirement import Requirement
---
+++ tracker/models/llmrequestlog.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+
+class LlmRequestLog(models.Model):
+    class CallType(models.TextChoices):
+        PARSE_JD = "parse_jd", "Parse Job Description"
+        RESUME_BULLETS = "resume_bullets", "Resume Bullets"
+        RESUME_SKILLS = "resume_skills", "Resume Skills"
+
+    timestamp = models.DateTimeField(default=timezone.now)
+    call_type = models.CharField(max_length=32, choices=CallType.choices)
+    model = models.CharField(max_length=64)
+    input_tokens = models.PositiveIntegerField()
+    output_tokens = models.PositiveIntegerField()
+
+    def total_tokens(self):
+        return self.input_tokens + self.output_tokens
