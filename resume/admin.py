ffabbe578e6d7a89a279f4a4b6cf3eadc10311d8
Victor Hidrogo <vhidrogo@uci.edu>
2025-11-05 11:12:28 -0800

    feat: add ExperinceRoleAdmin

--- resume/admin.py
+++ resume/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ResumeTemplate, TemplateRoleConfig
+from .models import ExperienceRole, ResumeTemplate, TemplateRoleConfig
 
+admin.site.register(ExperienceRole)
 admin.site.register(ResumeTemplate)
 admin.site.register(TemplateRoleConfig)
