8a96da0ed096548e19b9e66c4c13a7da954b36f6
Victor Hidrogo <vhidrogo@uci.edu>
2025-11-05 11:08:40 -0800

    feat: add ResumeTemplate admin

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ResumeTemplate, TemplateRoleConfig
 
-# Register your models here.
+admin.site.register(ResumeTemplate)
+admin.site.register(TemplateRoleConfig)
