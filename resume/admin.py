06fb37a861e7995f507d9e0603e4277caf496469
Victor Hidrogo <vhidrogo@uci.edu>
2025-11-05 14:48:41 -0800

    feat: add ResumeTemplate admin with ResumeTemplateConfig inline

--- resume/admin.py
+++ resume/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import ExperienceRole, ResumeTemplate, TemplateRoleConfig
 
+
+class TemplateRoleConfigInline(admin.TabularInline):
+    model = TemplateRoleConfig
+
+
+class ResumeTemplateAdmin(admin.ModelAdmin):
+    inlines = [
+        TemplateRoleConfigInline,
+    ]
+
+
 admin.site.register(ExperienceRole)
-admin.site.register(ResumeTemplate)
+admin.site.register(ResumeTemplate, ResumeTemplateAdmin)
 admin.site.register(TemplateRoleConfig)
