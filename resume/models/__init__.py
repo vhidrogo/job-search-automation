31087f47219b6c9ea6784f7a22076134c3d69168
Victor Hidrogo <vhidrogo@uci.edu>
2025-11-12 22:15:33 -0800

    refactor: remove dead code

--- resume/models/__init__.py
+++ resume/models/__init__.py
@@ -3,8 +3,6 @@ from .experience_role import ExperienceRole
 from .resume import Resume
 from .resume_role import ResumeRole
 from .resume_role_bullet import ResumeRoleBullet
-from .resume_experience_bullet import ResumeExperienceBullet
 from .resume_skills_category import ResumeSkillsCategory
-from .resume_skill_bullet import ResumeSkillBullet
 from .resume_template import ResumeTemplate, StylePath, TargetSpecialization, TemplatePath
 from .template_role_config import TemplateRoleConfig
