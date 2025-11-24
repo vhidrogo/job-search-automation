66f0e73ae7484e5f70505b2ec7edccb7b1c5e8a1
Victor Hidrogo <vhidrogo@uci.edu>
2025-11-23 19:42:59 -0800

    fix: use timezone aware datetime to display applied date in application admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from .models import (
     Application,
@@ -41,7 +42,7 @@ class ApplicationAdmin(admin.ModelAdmin):
 
 
     def applied_date_no_time(self, obj):
-        return obj.applied_date.date()
+        return timezone.localtime(obj.applied_date).date()
     applied_date_no_time.short_description = 'Applied Date'
 
 
