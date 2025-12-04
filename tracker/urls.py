2187acfa3624a1ff5d8772a21e599b004abec404
Victor Hidrogo <vhidrogo@uci.edu>
2025-12-03 17:01:13 -0800

    feat: add application detail view

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from tracker.views import upcoming_interviews
+from tracker.views import application_detail, upcoming_interviews
 
 
 app_name = 'tracker'
 
 urlpatterns = [
+    path('applications/<int:pk>/', application_detail, name='application_detail'),
     path('interviews/upcoming/', upcoming_interviews, name='upcoming_interviews'),
 ]
--- tracker/views/__init__.py
+++ tracker/views/__init__.py
@@ -1,1 +1,2 @@
+from .application_detail import application_detail
 from .interviews import upcoming_interviews
