a1b9b7eb51face1555ebef9875c25b948060bee1
Victor Hidrogo <vhidrogo@uci.edu>
2025-12-09 00:05:07 -0800

    feat: add stage filter in interview admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -78,6 +78,7 @@ class ContractJobAdmin(admin.ModelAdmin):
 class InterviewAdmin(admin.ModelAdmin):
     autocomplete_fields = ['application']
     list_display = ['application', 'stage', 'format', 'focus', 'scheduled_at']
+    list_filter = ['stage']
     formfield_overrides = {
         models.TextField: {'widget': forms.Textarea(attrs={'rows': 20, 'cols': 60})},
     }
