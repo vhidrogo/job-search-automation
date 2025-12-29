d68f900cce759af95ee65f76e935d62c61efc02f
Victor Hidrogo <vhidrogo@uci.edu>
2025-12-29 12:32:38 -0800

    feat: add JobListing admin with actions to mark status

--- jobs/admin.py
+++ jobs/admin.py
@@ -16,3 +16,31 @@ class CompanyAdmin(admin.ModelAdmin):
         if obj and obj.platform == Company.Platform.WORKDAY:
             return [WorkdayConfigInline(self.model, self.admin_site)]
         return []
+
+
+@admin.register(JobListing)
+class JobListingAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "company",
+        "location",
+        "status",
+        "is_stale",
+        "last_fetched",
+    ]
+    search_fields = ["title", "location", "external_id"]
+    readonly_fields = ["last_fetched"]
+
+    actions = ["mark_as_interested", "mark_as_dismissed", "mark_as_applied"]
+
+    @admin.action(description="Mark selected jobs as interested")
+    def mark_as_interested(self, request, queryset):
+        queryset.update(status=JobListing.Status.INTERESTED)
+
+    @admin.action(description="Mark selected jobs as dismissed")
+    def mark_as_dismissed(self, request, queryset):
+        queryset.update(status=JobListing.Status.DISMISSED)
+
+    @admin.action(description="Mark selected jobs as applied")
+    def mark_as_applied(self, request, queryset):
+        queryset.update(status=JobListing.Status.APPLIED)
