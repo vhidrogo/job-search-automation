9e3b31864e6085c5abfabca3669d25a21df974f6
Victor Hidrogo <vhidrogo@uci.edu>
2025-12-29 11:23:39 -0800

    feat: add Company admin with platform config inline

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from jobs.models import Company, WorkdayConfig, JobListing
 
-# Register your models here.
+
+class WorkdayConfigInline(admin.StackedInline):
+    model = WorkdayConfig
+    can_delete = False
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["name", "platform", "active"]
+    list_filter = ["platform", "active"]
+    search_fields = ["name"]
+
+    def get_inline_instances(self, request, obj=None):
+        if obj and obj.platform == Company.Platform.WORKDAY:
+            return [WorkdayConfigInline(self.model, self.admin_site)]
+        return []
