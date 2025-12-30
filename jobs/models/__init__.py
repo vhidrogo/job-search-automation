369b508c52522c50f2d7351fef94c0464bd1ce60
Victor Hidrogo <vhidrogo@uci.edu>
2025-12-30 12:00:48 -0800

    refactor: remove SearchRoleConfig

--- jobs/models/__init__.py
+++ jobs/models/__init__.py
@@ -1,6 +1,4 @@
-from .choices import SearchRole
 from .company import Company
 from .job_listing import JobListing
 from .search_config import SearchConfig
-from .search_role_config import SearchRoleConfig
 from .workday_config import WorkdayConfig
